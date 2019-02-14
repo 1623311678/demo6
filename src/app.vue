b9274a4a4838d27b388b83726a9c1fd18c3328c5
1623311678 <1623311678@qq.com>
2019-02-14 19:05:34 +0800

    'scroll'

--- src/app.vue
+++ src/app.vue
@@ -1,16 +1,8 @@
 <template>
   <div>
-    <Scroll
-      :on-reach-bottom="handleReachBottom"
-      :on-reach-top="handleReachTop"
-      :height="750"
-      class="demo-scroll"
-    >
-      <Card dis-hover v-for="(item, index) in list1" :key="index">
-        <div class="demo-content">Content {{ item }}</div>
-      </Card>
-      <div class="demo-scroll-tip">下拉加载更多</div>
-    </Scroll>
+    <div class="demo-scroll">
+      <div class="demo-content" v-for="(item, index) in list1" :key="index">Content {{ item }}</div>
+    </div>
   </div>
 </template>
 <script>
@@ -18,41 +10,58 @@ export default {
   name: "APP",
   data() {
     return {
-      list1: [1, 2, 3, 4, 5, 6, 7, 8, 9, 10]
+      list1: [],
+      bottom: false
     };
   },
   methods: {
-    handleReachTop() {//下拉刷新页面
-    return new Promise(resolve => {
-      setTimeout(() => {
+    bottomVisible() {
+      const scrollY = document.documentElement.scrollTop;
+      const visible = window.innerHeight;
+      const pageHeight = document.documentElement.offsetHeight;
+      const bottomOfPage = visible + scrollY >= pageHeight;
+      if (scrollY < 10) {
         window.location.reload();
-        resolve();
-      }, 2000);
-    });
-  },
-    handleReachBottom() {//上啦加载更多
-      return new Promise(resolve => {
-        setTimeout(() => {
-          for (let i = 0; i < 2; i++) {
-            this.list1.push(i);
-          }
-          resolve();
-        }, 2000);
-      });
+      }
+      return bottomOfPage || pageHeight < visible;
+    },
+
+    addBeer() {
+      for (let i = 0; i < 10; i++) {
+        this.list1.push(i);
+      }
     }
-  }
+  },
+  watch: {
+    bottom(bottom) {
+      if (bottom) {
+        this.addBeer();
+      }
+      // if(!bottom ){
+      //   window.location.reload();
+      //   console.log('shangla',shangla)
+
+      // }
+    }
+  },
+  created() {
+    window.addEventListener("scroll", () => {
+      this.bottom = this.bottomVisible();
+      console.log("this.bottom", this.bottom);
+    });
+    this.addBeer();
+  },
+  mounted() {}
 };
 </script>
 <style lang="less">
 .demo-scroll {
+  overflow: hidden;
+  background: green;
   .demo-content {
-    height: 0.5rem;
+    border: 1px solid black;
+    height: 2rem;
     color: red;
-    font-size: 0.5rem;
-    text-align: center;
-    line-height: 0.5rem;
-  }
-  .demo-scroll-tip {
     font-size: 0.5rem;
     text-align: center;
     line-height: 0.5rem;
